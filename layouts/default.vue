ee0af279483b02352c8d74ec91d2efa4a549d139
RaulRivas302 <120042712+RaulRivas302@users.noreply.github.com>
2022-12-09 01:22:13 +0500

    update

---
+++ layouts/default.vue
@@ -0,0 +1,28 @@
+<template>
+  <b-container class="b-container">
+    <b-row class="my-5">
+      <b-col lg="8">
+        <h3>Company Inc.</h3>
+      </b-col>
+      <b-col>
+        <NuxtLink to="/">Home</NuxtLink>
+        <NuxtLink to="pricing" class="ml-4">Pricing</NuxtLink>
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col>
+        <Nuxt />
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: 'Default',
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/pricing.vue
@@ -0,0 +1,40 @@
+<template>
+  <b-container class="text-center">
+    <b-row>
+      <b-col class="mt-5">
+        <h1>Pricing</h1>
+      </b-col>
+    </b-row>
+    <b-row class="justify-content-center">
+      <b-col md="8">
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo,
+          tenetur. Eius, fugit quidem tempore, quia officia impedit possimus
+          officiis doloremque autem illum reiciendis libero dignissimos
+          repellendus dolores veniam error odit!
+        </p>
+      </b-col>
+    </b-row>
+      <PricingCard/>
+  </b-container>
+</template>
+
+<script>
+
+import PricingCard from '../components/PricingCard.vue';
+export default {
+  components: { PricingCard },
+  name: 'Pricing',
+  layout: 'default',
+  computed: {
+    PricingCard,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+p {
+  color: #6c757d;
+  font-size: 1.25rem;
+}
+</style>
